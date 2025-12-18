57d166ca84c1e0223b87081443546b0ea7e0b8d8
Tusu122 <tusu091@gmail.com>
2025-12-19 02:57:16 +0700

    asc

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -34,9 +34,8 @@ class ProductsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show()
+    public function show(Products $product)
     {
-        $product = Products::OrderBy('id','ASC');
         return response()->json([
             'status' => 200,
             'product' => $product
